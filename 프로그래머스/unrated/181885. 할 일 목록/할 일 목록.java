9ce61d109704adcf8efc7656b26f3070d9703114
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-19 17:12:27 +0900

    [level unrated] Title: 할 일 목록, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answerList = new ArrayList<>();
+        for(int i = 0; i < finished.length; i++)
+            if(!(finished[i])){
+                answerList.add(todo_list[i]);
+            }
+        String[] answer = new String[answerList.size()];
+        return answer = answerList.toArray(answer);
+    }
+}
