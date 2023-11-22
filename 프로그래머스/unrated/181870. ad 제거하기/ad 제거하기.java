8bd849f0422ca5657a2b1199bb3898c61e9dd7d5
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-11-22 11:15:32 +0900

    [level unrated] Title: ad 제거하기, Time: 1.01 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> answer = new ArrayList<String>();
+        for(int i = 0; i < strArr.length;i++)
+            if(!strArr[i].contains("ad"))
+                answer.add(strArr[i]);
+        return answer.toArray(new String[answer.size()]);
+    }
+}
