5d2f54b95bcb400e0447bce689a10676d3c87d6f
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2023-12-13 22:46:03 +0900

    [level unrated] Title: 평행, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double inclination(int dot1[], int dot2[]){
+        return (double)(dot2[1]-dot1[1])/(dot2[0]-dot1[0]);
+    }
+    
+    public int solution(int[][] dots) {
+        if(inclination(dots[0],dots[1]) == inclination(dots[2],dots[3]))
+            return 1;
+        if(inclination(dots[0],dots[2]) == inclination(dots[1],dots[3]))
+            return 1;
+        if(inclination(dots[0],dots[3]) == inclination(dots[1],dots[2]))
+            return 1;
+        return 0;
+    }
+}
